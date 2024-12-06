677cef5220523ce42a28f2666da334b5d86ee03e
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2024-12-05 21:20:11 -0500

    Clean up day 5

--- src/day5_part1.py
+++ src/day5_part1.py
@@ -14,33 +14,20 @@ for line in lines:
         section_one = False
     else:
         if section_one:
-            splitted = line.split("|")
-
-            if len(splitted) < 2:
-                print("improper")
-                break
-
-            first = int(splitted[0])
-            second = int(splitted[1])
+            first, second = line.split("|")
 
             if first not in page_rules:
-                pages = set()
-                pages.add(second)
-                page_rules[first] = pages 
+                page_rules[first] = set([second]) 
             else:
                 page_rules[first].add(second)
         else:
             # now we dealing with the actual pages
-            pages = line.split(",")
+            pages = list(int(x) for x in line.split(","))
 
             correct = True 
 
             for curr_i, curr_page in enumerate(pages):
-                curr_page = int(curr_page)
-
                 for next_page in pages[curr_i + 1:]:
-                    next_page = int(next_page)
-
                     if next_page in page_rules:
                         if curr_page in page_rules[next_page]:
                             correct = False
--- src/day5_part2.py
+++ src/day5_part2.py
@@ -11,37 +11,24 @@ section_one = True
 
 for line in lines:
     if len(line) == 0:
-        print(page_rules)
         section_one = False
     else:
         if section_one:
-            splitted = line.split("|")
-
-            if len(splitted) < 2:
-                print("improper")
-                break
-
-            first = int(splitted[0])
-            second = int(splitted[1])
+            first, second = line.split("|")
+            first, second = int(first), int(second)
 
             if first not in page_rules:
-                pages = set()
-                pages.add(second)
-                page_rules[first] = pages 
+                page_rules[first] = set([second]) 
             else:
                 page_rules[first].add(second)
         else:
             # now we dealing with the actual pages
-            pages = line.split(",")
+            pages = list(int(x) for x in line.split(","))
 
             correct = True 
 
             for curr_i, curr_page in enumerate(pages):
-                curr_page = int(curr_page)
-
                 for next_page in pages[curr_i + 1:]:
-                    next_page = int(next_page)
-
                     if next_page in page_rules:
                         if curr_page in page_rules[next_page]:
                             correct = False
@@ -55,23 +42,12 @@ for line in lines:
                 while swapped:
                     swapped = False
                     for i in range(1, n):
-                        left_page = int(pages[i - 1])
-                        right_page = int(pages[i])
-                        
-                        # swap
-                        # left | right
-                        # if left is not in right
-                        # 
-                        # and right is not in the page_rules 
-                        
+                        left_page, right_page = pages[i - 1], pages[i]
 
                         if left_page not in page_rules or (right_page not in page_rules[left_page] and right_page in page_rules.keys()):
-                            tmp_page = pages[i]
-                            pages[i] = left_page
-                            pages[i - 1] = right_page
+                            pages[i - 1], pages[i] = right_page, left_page
                             swapped = True
 
-                print(pages)
 
                 out += int(pages[len(pages) // 2])
 
